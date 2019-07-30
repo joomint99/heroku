a9fe2ca999b65b4f047641de45b29afb85384f0f
LeeGyuWon <joomint99@naver.com>
2019-07-30 20:43:28 +0900

    asdf

--- myvenv/Scripts/Activate.ps1
+++ myvenv/Scripts/Activate.ps1
@@ -27,7 +27,7 @@ function global:deactivate ([switch]$NonDestructive) {
 
 deactivate -nondestructive
 
-$env:VIRTUAL_ENV="C:\Users\joomi\OneDrive\바탕 화면\model\myvenv"
+$env:VIRTUAL_ENV="C:\Users\USER-PC\Desktop\faker\myvenv"
 
 if (! $env:VIRTUAL_ENV_DISABLE_PROMPT) {
     # Set the prompt to include the env name
